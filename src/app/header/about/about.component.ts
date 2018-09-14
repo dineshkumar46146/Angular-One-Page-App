ed3d17d9f5563332664065be33f35881cf2c234f
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-09-15 00:32:15 +0530

    One page application in angular

--- src/app/header/about/about.component.ts
+++ src/app/header/about/about.component.ts
@@ -6,11 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  action2 = 'about';
-  setAction(action){
-   
-    this.action2 = action;
-  }
   constructor() { }
 
   ngOnInit() {
